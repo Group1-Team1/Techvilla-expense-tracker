a8c3baec7e5057d965bcdec81c2d392b1a92200a
ibrahim <hassan.ibb120044@gmail.com>
2022-08-24 10:35:12 +0100

    Getting prepared for the presentation.

---
+++ js/javaScript.js
@@ -0,0 +1,15 @@
+alert("welcome to TECHVILLA EXPENCE TRACKER!")
+
+let myPage = document.getElementById("page")
+console.log(myPage)
+
+let myHeading = document.getElementById("heading")
+
+let greeting = ("")
+console.log(greeting)
+
+techville = () => {
+      greeting= "WELCOME ON BOARD TO OUR EXPENCE TRACKER!"
+      myHeading.textContent = greeting
+}
+techville.textContent = myPage
