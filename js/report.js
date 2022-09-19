90aa3bec666c1fe88e987362ddbfb044f1a5b52e
Ibrahim Hassan <hassan.ibb120044@gmail.com>
2022-09-19 10:32:20 +0100

    Report screen tracker
    This is the report screen for the expense tracker web page.

---
+++ js/report.js
@@ -0,0 +1,25 @@
+const barSlide = () => {
+    const burger = document.querySelector("#hamburger");
+    const nav = document.querySelector("#bar");
+    const navBar = document.querySelectorAll("#bar li");
+  
+    burger.addEventListener("click", () => {
+      // to toggle
+      nav.classList.toggle("option-active");
+  
+      // to animate
+      navBar.forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = "";
+        } else {
+          link.style.animation = `newIcon 0.5s ease forwards ${index / 5 + 0.2}s`;
+        }
+        
+      });
+      
+      // burger animation
+      burger.classList.toggle("toggle");
+    });
+  };
+  
+  barSlide();
